da39a79e1ceab61cc67199f0858c6efb72a5eac9
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-26 14:42:51 +0900

    d

---
+++ apps/addint/src/App.js
@@ -0,0 +1,25 @@
+export default function App() {
+  return (
+    <Toolbar
+      onPlayMovie = {() => alert('playing!')}
+      onUploadImage = {() => alert('Uploading!')}
+    />
+  );
+}
+
+function Toolbar({ onPlayMovie, onUploadImage}) {
+  return (
+    <div>
+      <Button onClick={onPlayMovie}>Play Movie</Button>
+      <Button onClick={onUploadImage}>Upload Image</Button>
+    </div>
+  );
+}
+
+function Button({ onClick, children }) {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  );
+}
