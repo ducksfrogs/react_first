45aca591011dd2b7efcfda6468ce040b4774e6fd
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-26 10:38:51 +0900

    setting fin

---
+++ apps/describing_ui/src/Gallery.js
@@ -0,0 +1,13 @@
+import Profile from "./Profile.js";
+
+export default function Gallery() {
+    return (
+        <section>
+            <h1>Amazing scientist </h1>
+            <Profile />
+            <Profile />
+            <Profile />
+
+        </section>
+    )
+}
