07510cb87fa6271eb7bde39a2c2797707d2819a5
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-28 09:16:45 +0900

    expense data

--- apps/udemy/src/components/ExpenseItem.js
+++ apps/udemy/src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-
     return(
+        <div>
+        <ExpenseDate date={props.date}/>
         <div className="momon">
-            <div>{props.date.toISOString()}</div>
-            <div>
-                <h2>{props.title}</h2>
-                <div>{props.amount}</div>
-            </div>
+            <h2>{props.title}</h2>
+            <div>{props.amount}</div>
+        </div>
         </div>
     );
 }
