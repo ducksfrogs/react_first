20f7f11545eccf7038b6db588bda1e488921ca7d
ducksfrogs <ma_yamaki@yahoo.com>
2023-06-01 06:24:18 +0900

    dafdasfasf

--- apps/udemy/src/App.js
+++ apps/udemy/src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewExpense from './components/NewExpenses/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
-function App() {
-  const expenses = [
-    { id: '1', title: 'momon', amount: 25.56, date: new Date(2021, 2,20)},
-    { id: '2', title: 'toiret paper', amount: 295.56, date: new Date(2021, 2,20)},
-    { id: '3', title: 'kikij', amount: 95.56, date: new Date(2021, 2,20)},
-    { id: '4', title: 'Car instrance', amount: 29.56, date: new Date(2021, 2,20)},
-    { id: '5', title: 'Car instrance', amount: 29.56, date: new Date(2021, 2,20)},
-  ];
+const DUMMY_EXPENSES = [
+  { id: '1', title: 'momon', amount: 25.56, date: new Date(2021, 2,20)},
+  { id: '2', title: 'toiret paper', amount: 295.56, date: new Date(2021, 2,20)},
+  { id: '3', title: 'kikij', amount: 95.56, date: new Date(2021, 2,20)},
+  { id: '4', title: 'Car instrance', amount: 29.56, date: new Date(2021, 2,20)},
+  { id: '5', title: 'Car instrance', amount: 29.56, date: new Date(2021, 2,20)},
+];
 
-  const addExpenseHandler = (expenses) => {
-    console.log('In App.js');
-    console.log(expenses);
-  }
+
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = expense => {
+    // setExpenses([expense, ...expenses``])
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
